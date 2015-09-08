e9c95c6e3552f337039730715fad26ed8dcd2594
Joseph Tresca <jtresca@glgroup.com>
2015-09-08 15:17:19 -0400

    stripe module added outside of build wire so it doesn't get stripped

--- server/api/charge/index.js
+++ server/api/charge/index.js
@@ -7,7 +7,7 @@ var auth = require('../../auth/auth.service');
 var router = express.Router();
 
 
-router.post('/', auth.hasRole('admin') ,controller.charge);
+router.post('/',controller.charge);
 
 
 module.exports = router;
